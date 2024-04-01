0d7239324436b618d16ea9745f92ece9c1fbef64
Michelle Chi <mgchi@calpoly.edu>
2024-04-01 15:24:36 -0700

    tabs and toggle menu done. things kinda broken

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/michelle-chi/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin"></i>
+            </a>
+            <a href="" className="home__social-icon" target="_blank">
+                <i class="uil uil-dribble"></i>
+            </a>
+            <a href="https://github.com/michellechi" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Home
