9b4f4aeee5c395a747c6b8913db64018755eff65
Michelle Chi <mgchi@calpoly.edu>
2024-04-02 00:42:18 -0700

    header works, not dark mode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,55 +1,134 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 
 const Header = () => {
-    const[Toggle, showMenu] = useState(false);
+  const [Toggle, showMenu] = useState(false);
 
-    return (
-        <header className='header'>
-            <nav className='nav container'>
-                <a href="index.html" className="nav__logo">Michelle Chi</a>
-                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-                    <ul className="nav__list grid">
-                        <li className='nav__item'>
-                            <a href='#home' className='nav__link active-link'>
-                                <i className='uil uil-estate nav__icon'>Home</i>
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#about' className='nav__link'>
-                                <i className='uil uil-user nav__icon'>About</i>
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#skills' className='nav__link'>
-                                <i className='uil uil-file-alt nav__icon'>Skills</i>
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#services' className='nav__link'>
-                                <i className='uil uil-briefcase-alt nav__icon'>Services</i>
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#portfolio' className='nav__link'>
-                                <i className='uil uil-scenery nav__icon'>Portfolio</i>
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#contact' className='nav__link'>
-                                <i className='uil uil-message nav__icon'>Contact</i>
-                            </a>
-                        </li>
-                    </ul>
+  const [activeNav, setActiveNav] = useState("#home");
 
-                    <i className='uil uil-times nav__close' onClick={() => showMenu(!Toggle)}></i>
-                </div>
-                <div className='nav__toggle' onClick={() => showMenu(!Toggle)}>
-                    <i class='uil uil-apps'></i>
-                </div>
-            </nav>
-        </header>
-    )
-}
+  const [theme, setTheme] = useState("light_theme");
 
-export default Header
+  const [li, setli] = useState("bx bxs-moon");
+  // let li = "";
+  // let di = "bx bx-sun";
+
+  const toggleTheme = () => {
+    if (theme === "light_theme") {
+      setTheme("dark_theme");
+      setli("bx bxs-sun");
+    } else {
+      setTheme("light_theme");
+      setli("bx bxs-moon");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  return (
+    <header className="header" id="header">
+      <nav className="nav container">
+        <a href="#home" className="nav__logo">
+          Michelle Chi
+        </a>
+
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
+                }
+              >
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={
+                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
+                }
+              >
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#projects"
+                onClick={() => setActiveNav("#projects")}
+                className={
+                  activeNav === "#projects"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-scenery nav__icon"></i> Projects
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-file-alt nav__icon"></i> Resume
+              </a>
+            </li>
+            <li className="nav__item">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-file-alt nav__icon"></i> Contact
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <div className="home__social-icon nav__icontoggle" onClick={toggleTheme}>
+                <i class={li}></i>
+              </div>
+            </li>
+          </ul>
+
+          <i
+            class="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
+
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i class="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
