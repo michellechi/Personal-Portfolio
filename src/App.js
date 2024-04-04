84508ab0f3d724b13c20622fae4de49dab86c556
Michelle Chi <mgchi@calpoly.edu>
2024-04-04 14:19:35 -0700

    contact page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Skills from './components/skills/Skills';
 import Work from './components/projects/Work';
 import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
-
-
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -21,6 +20,7 @@ const App = () => {
       <Work />
       <Qualification />
       <Contact />
+      <Footer />
     </main>
     </>
   )
