d8865e20bbe51c22553ca99666314217125c0f47
Michelle Chi <mgchi@calpoly.edu>
2024-10-10 09:27:27 -0700

    updated resume and projects

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,63 +1,63 @@
-import altcoinx from "../../assets/work1.jpg";
-import ondrive from "../../assets/work2.jpg";
-import webshop from "../../assets/work3.jpg";
-import notesApp from "../../assets/work4.jpg";
-import chatzen from "../../assets/work5.jpg";
-import socialhub from "../../assets/work1.jpg";
+import polybuddy from "../../assets/work1.jpg";
+import library from "../../assets/work2.jpg";
+import game from "../../assets/work3.jpg";
+import bitmap from "../../assets/work4.jpg";
+import simulation from "../../assets/work5.jpg";
+import jackrabbit from "../../assets/work1.jpg";
 
 export const projectsData = [
   {
     id: 1,
-    image: chatzen,
+    image: polybuddy,
     title: "PolyBuddy",
     description:
-      "Chatzen is a user-friendly chat application project. It provides an intuitive platform for users to connect and engage in conversations.",
+      "PolyBuddy is a mobile application project. It provides an intuitive platform aimed to seamlessly guide students and faculty through the Cal Poly SLO campus, enhacing overall campus experience.",
     demo: "https://mango-coast-050270d10.5.azurestaticapps.net/",
     code: "https://github.com/michellechi/PolyBuddy-App",
   },
   {
     id: 2,
-    image: socialhub,
+    image: library,
     title: "Library Management App",
     description:
-      "SocialHub is a social media platform. It provides an intuitive space to and build connections and share content with friends and colleagues.",
+      "Libaray Management App is a user-friendly interface for users and admin. It has key functionalities including login, book management, returns, search, and automated fine capabilities.",
     demo: "",
     code: "https://github.com/michellechi/Library-Management-System",
   },
   {
     id: 3,
-    image: notesApp,
+    image: game,
     title: "2048 Game",
     description:
-      "Notes App is a streamlined markdown note-taking platform. It boasts an intuitive interface, modern design, and effortless navigation.",
+      "This is a personal rendition of the classic game 2048.",
     demo: "",
     code: "https://github.com/michellechi/2048-PyGame",
   },
   {
     id: 4,
-    image: webshop,
+    image: bitmap,
     title: "Bitmap Image Encoding/Decoding",
     description:
       "Webshop is an e-commerce website with intuitive design and a simple shopping experience that puts focus on the products.",
-    demo: "https://webshop-ebon.vercel.app/",
+    demo: "",
     code: "https://github.com/michellechi/BMP-Image-Encoding-Decoding",
   },
   {
     id: 5,
-    image: altcoinx,
+    image: simulation,
     title: "Interactive Game Simulation",
     description:
       "Altcoinx is a website that allows users to access real-time information about various cryptocurrencies. ",
-    demo: "https://altcoinx-one.vercel.app/",
+    demo: "",
     code: "https://github.com/michellechi/InteractiveVirtualSimulation",
   },
   {
     id: 6,
-    image: ondrive,
-    title: "Ondrive",
+    image: jackrabbit,
+    title: "Jackrabbit Marketplace",
     description:
       "Ondrive is an online platform with easy-to-use interface that allows users to rent cars for personal or business use.",
-    demo: "https://ondrive-plum.vercel.app/",
-    code: "https://github.com/pvictordev/ondrive",
+    demo: "https://www.jackrabt.com/",
+    code: "",
   },
 ];
